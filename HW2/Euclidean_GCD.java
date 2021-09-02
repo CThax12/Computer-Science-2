6ccbe8d1c7d012a2c9cfea464ea1eaa8402630ba
CThax12 <89363137+CThax12@users.noreply.github.com>
2021-09-01 22:34:12 -0500

    Update Euclidean_GCD.java

--- HW2/Euclidean_GCD.java
+++ HW2/Euclidean_GCD.java
@@ -1,27 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Euclidean_GCD {
 	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("Please enter two integers to find the GCD of. \n");
+		System.out.println("Please enter a list of integers to find the GCD of. \n"
+				+ "One per line.");
 		
-		int firstNum = scnr.nextInt();
-		int secondNum = scnr.nextInt();
+		ArrayList<Integer> numList = new ArrayList<Integer>();
 		
-		int gcdResult = gcd(firstNum, secondNum);
-		System.out.println(gcdResult);
+		while (scnr.hasNextInt()) {
+			numList.add((scnr.nextInt()));
+		}
+		
+		int gcdResult = gcd(numList, numList.get(0), numList.get(1));
+		System.out.println("The GCD of these numbers is: " + gcdResult);
 	}
 	
-	/* Euclid's Algorithm works by first dividing the two numbers. The divisor becomes the dividend. 
+	/* Euclid's Algorithm works by first dividing the two numbers. The divisor(b) becomes the dividend(a). 
 	 * Then, the remainder becomes the divisor. The process repeats itself until it gets a remainder of 0.
-	 * This indicates that it has found the GCD of the two numbers requested.
+	 * This indicates that it has found the GCD of the numbers requested.
 	 */
-	public static int gcd(int a, int b) {
-		if (a == 0) {
+	public static int gcd(ArrayList<Integer> nums, Integer a, Integer b) {
+		if (a == 0 && nums.size() > 1) {
+			nums.remove(0);
+			return gcd(nums, b, nums.get(0));
+		}
+		
+		if (a == 0 && nums.size() <= 1) {
 			return b;
 		}
-			return gcd(b%a, a);
+		else {
+			return gcd(nums, b%a, a);
+		}
 		
 	}
 
