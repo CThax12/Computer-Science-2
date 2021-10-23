b7f4da36dd4b7e9acfe8dbe632fad31ee2eae777
CThax12 <89363137+CThax12@users.noreply.github.com>
2021-10-22 22:48:04 -0500

    Create 12-10.py

---
+++ HW7/12-10.py
@@ -0,0 +1,37 @@
+file_name = input()
+user_file = open(str(file_name))
+output_list = user_file.readlines()
+my_dict = {}
+show_list = []
+show_list_split = []
+for i in range(len(output_list)):
+    temp_list = []
+    list_object = output_list[i].strip('\n')
+    if (i + 1 < len(output_list) and (i % 2 == 0)):
+        if int(list_object) in my_dict:
+            my_dict[int(list_object)].append(output_list[i + 1].strip('\n'))
+        else:
+            temp_list.append(output_list[i + 1].strip('\n'))
+            my_dict[int(list_object)] = temp_list
+            
+my_dict_sorted_by_keys = dict(sorted(my_dict.items()))
+for x in my_dict.keys():
+    show_list.append(my_dict[x])
+for x in show_list:
+    for i in x:
+        show_list_split.append(i)
+show_list_split = sorted(show_list_split)
+f = open('output_keys.txt', 'w')
+for key, value in my_dict_sorted_by_keys.items():
+    f.write(str(key) + ': ')
+    for item in value[:-1]:
+        f.write(item + '; ')
+    else:
+        f.write(value[-1])
+        f.write('\n')
+        
+f.close()
+f = open('output_titles.txt', 'w')
+for item in show_list_split:
+    f.write(item + '\n')
+f.close()
